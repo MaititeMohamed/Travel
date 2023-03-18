57ee92ce6aeea89031294f53067be223a6da6d97
MAITITE MOHAMED <maititemohamed@gmail.com>
2023-03-18 23:23:05 +0100

    create TravelBackEnd Project

---
+++ TravelBackEnd/src/test/java/com/example/travelbackend/TravelBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
