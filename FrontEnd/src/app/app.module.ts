9fbff5420ffeefb43f7242cb129657c3d24922f0
MAITITE MOHAMED <maititemohamed@gmail.com>
2023-04-08 21:13:39 +0000

    frontEnd

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -2,24 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './Pages/home/home.component';
-import { AboutComponent } from './Pages/about/about.component';
-
-import { RouterModule, Routes } from '@angular/router';
-
-
-const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
- 
-];
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
